cb170d7f5644b5037abe55119a14d2fdb1bfeaf4
btb63 <git@burkecode.com>
2019-04-11 17:34:59 -0500

    Make it Pretty, remove some more unused code.

--- src/App.js
+++ src/App.js
@@ -18,30 +18,24 @@ class App extends Component {
                           '1. This\n2. Is\n3. A\n4. List\n why does markdown do this.\n ### Huh?\n' +
                           '>This is what a blockquote looks like\n' +
                           '>What do you think of that? \n\n' +
-                          '#### And now, an image: ' +
-                          '![A Kitten](https://www.pets4homes.co.uk/images/articles/1646/large/kitten-emergencies-signs-to-look-out-for-537479947ec1c.jpg "Kitty")\n\n' +
+                          '#### And now, an image: \n' +
+                          '![A Spriggan!](https://www.btburke.com/spriggan.png "Don\'t Drop it!")\n\n' +
                           '**Finally, bold text.**'
                         };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
     this.setState({value: event.target.value})
   }
 
-  handleSubmit(event) {
-    let renderedMd = marked(this.state.value)
-    alert('Parsed MD:  ' + renderedMd);
-    event.preventDefault();
-  }
 
   render() {
     return (
-      <div className="App row">
-        <header>
-          <h1 className="header">Markdown Previewer</h1>
+      <div className="App">
+        <header className="header">
+          <h1>Markdown Previewer</h1>
         </header>
         <body>
           <textarea className="input-section" id="editor" value={this.state.value} onChange={this.handleChange}
